50a9477890b21da0a4d6515fd209ae1332e3c6de
Micael <micaelmoura.oliveira@gmail.com>
2023-01-27 12:47:12 -0300

    Alterações nos comentários e nos posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,68 @@
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 
-import styles from './App.module.css';
-import './global.css';
+import styles from "./App.module.css";
+import "./global.css";
 import { Post } from "./components/Post";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/micaelmoliveira.png",
+      name: "Micael Oliveira",
+      position: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera" },
+      {
+        type: "paragraph",
+        content: "Acabei de subir mais um projeto no meu portifólio.",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-01-26 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/micaelmoliveira.png",
+      name: "João da Silva",
+      position: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera" },
+      {
+        type: "paragraph",
+        content: "Acabei de subir mais um projeto no meu portifólio.",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-01-15 15:00:00"),
+  },
+];
+
 function App() {
-  return(
+  return (
     <div>
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,48 +1,78 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Comment } from "./Comment";
 import { Avatar } from './Avatar';
 
 import styles from "./Post.module.css";
+import { useState } from 'react';
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState('');
+
+  const publisedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  })
+
+  const publisedDateRelativeNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
+  function handleCreateNewComment(event) {
+    event.preventDefault()
+    setComments([...comments, newComment]);
+    setNewComment('');
+  }
+
+  function handleNewComment(event){
+    setNewComment(event.target.value);
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar hasBorder src="http://github.com/micaelmoliveira.png" />
+          <Avatar hasBorder src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Jane Foster</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.position}</span>
           </div>
         </div>
-        <time title="11 de Janeiro às 16:50h" dateTime="2022-05-11 16:50:00">
-          Publicado há 1 hora
+        <time title={publisedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publisedDateRelativeNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galera</p>
-        <p>Esse é um post de teste</p>
-        <p>
-          <a href="">jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="">#novoprojeto</a> <a href="">#hashtag</a>{" "}
-          <a href="">#kkk</a>{" "}
-        </p>
+        {content.map(item => {
+          if(item.type === 'paragraph'){
+            return <p key={item.content}>{item.content}</p>
+          } else if(item.type === 'link') {
+            return <p key={item.content}><a href="#">{item.content}</a></p>
+          }
+        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={event => handleCreateNewComment(event)} className={styles.commentForm}>
         <strong>Deixe seu comentário</strong>
-        <textarea placeholder="Deixe um comentário" />
+        <textarea 
+          placeholder="Deixe um comentário" 
+          name="comment" 
+          value={newComment}
+          onChange={event => handleNewComment(event)}
+        />
         <footer>
           <button type="submit">Publicar</button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment key={comment} content={comment}/>
+        })}
       </div>
     </article>
   );
